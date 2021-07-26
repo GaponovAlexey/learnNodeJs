7022e3e7684297eac68f81e2cc748d0d8e17147d
Kekelberi <alexei.gaponov@gmail.com>
2021-07-26 22:52:55 +0300

    don't work

--- index.js
+++ index.js
@@ -1,10 +1,83 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Length': 'utf-8',
-		'Content-Type': 'text/html'})
 
-	res.end(`<h1>hello nodejs Yes</h1>`)})
+	//if (req.url === '/') {
+	//	fs.readFile(path.join
+	//		(__dirname, 'publick', 'index.html'),
+	//		(err, data) => {
+	//			if (err) { throw err }
 
-	server.listen(3000, () => {
-	console.log('server start');})
+	//			res.writeHead(200, {
+	//				'Content-Length': 'utf-8',
+	//				'Content-Type': 'text/html'
+	//			})
+	//			res.end(data)
+	//		})
+	//} else
+	//	if (req.url === '/contact') {
+	//		fs.readFile(path.join
+	//			(__dirname, 'publick', 'content.html'),
+	//			(err, data) => {
+	//				if (err) { throw err }
+
+	//				res.writeHead(200, {
+	//					'Content-Length': 'utf-8',
+	//					'Content-Type': 'text/html'
+	//				})
+	//				res.end(data)
+	//			})
+	//	}
+
+	let filePath = path.join(__dirname, 'publick', req.url === '/' ? 'index.html' : req.url)
+	
+	const ext = path.extname(filePath)
+	const contentType = 'text/html'
+	
+	switch (ext) {
+		case '.css':
+			contentType = 'text/css'
+			break
+		case '.js':
+			contentType = 'text/javascript'
+			break
+		default:
+			contentType = 'text/html'
+	}
+
+
+	if (!ext) {
+		filePath += '.html'
+	}
+
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			fs.readFile(path.join
+				(__dirname, 'publick', 'error.html'),
+				(err, data) => {
+					if (err) {
+						res.writeHead(500)
+						res.end('Error')
+					} else {
+						res.writeHead(200, {
+							'Content-Length': 'utf-8',
+							'Content-Type': contentType
+						})
+						res.end(data)
+					}
+				})
+		} else {
+			res.writeHead(200, {
+				'Content-Length': 'utf-8',
+				'Content-Type': contentType
+			})
+			res.end(content)
+		}
+	})
+})
+
+server.listen(3000, () => {
+	console.log('server starteddddd');
+})
